b13b2bf7df4fd341b04f09cb889cdbe1e63b7b48
snaipp1 <snaipp1@yandex.by>
2021-04-10 21:58:49 +0300

    feat: add basic interactivity

--- app.js
+++ app.js
@@ -6,41 +6,83 @@ const ROWS_COUNT = 5;
 const PADDING = 40;
 const VIEW_HEIGHT = DPI_HEIGHT - PADDING * 2;
 const VIEW_WIDTH = DPI_WIDTH;
+const CIRCLE_RADIUS = 8;
+
+const tgChart = chart(document.getElementById('chart'), getChartData());
+tgChart.init();
 
 function chart(canvas, data) {
+
   const ctx = canvas.getContext('2d');
+  let raf;
   canvas.style.width = WIDTH + 'px'; 
   canvas.style.height = HEIGHT + 'px'; 
   canvas.width = DPI_WIDTH;
   canvas.height = DPI_HEIGHT;
 
-  function mousemove() {
+  const proxy = new Proxy(
+    {}, 
+    {
+    set(...args) {
+      const result = Reflect.set(...args);
+      raf = requestAnimationFrame(paint)
+      return result;
+    },
+  });
 
+  function mousemove({clientX, clientY}) {
+    const {left} = canvas.getBoundingClientRect();
+    proxy.mouse = {
+      x: (clientX - left) * 2,
+    }
+  }
+
+  function mouseleave() {
+    proxy.mouse = null;
   }
 
   canvas.addEventListener('mousemove', mousemove);
+  canvas.addEventListener('mouseleave', mouseleave);
 
-  const [yMin, yMax] = computeBoundaries(data);
-  const yRatio = VIEW_HEIGHT / (yMax - yMin);
-  const xRatio = VIEW_WIDTH / (data.columns[0].length - 2);
+  function clear() {
+    ctx.clearRect(0, 0, DPI_WIDTH, DPI_HEIGHT);
+    
+  }
 
-  const yData = data.columns.filter(col => data.types[col[0]] === 'line');
-  const xData = data.columns.filter(col => data.types[col[0]] === 'x')[0];
-  console.log(xData)
+  function paint() {
+    clear();
+    const [yMin, yMax] = computeBoundaries(data);
+    const yRatio = VIEW_HEIGHT / (yMax - yMin);
+    const xRatio = VIEW_WIDTH / (data.columns[0].length - 2);
 
+    const yData = data.columns.filter(col => data.types[col[0]] === 'line');
+    const xData = data.columns.filter(col => data.types[col[0]] === 'x')[0];
 
+    yAxis(ctx, yMin, yMax);
+    xAxis(ctx, xData, xRatio, proxy);
 
-  yAxis(ctx, yMin, yMax);
-  xAxis(ctx, xData, xRatio);
+    yData.map(toCoords(xRatio, yRatio)).forEach((coords, idx) => {
+      const color = data.colors[yData[idx][0]];
+      line(ctx, coords, {color});
 
-  yData.map(toCoords(xRatio, yRatio)).forEach((coords, idx) => {
-    const color = data.colors[yData[idx][0]];
-    line(ctx, coords, {color});
-  });
+      for (const [x, y] of coords) {
+        if (isOver(proxy.mouse, x, coords.length)) {
+          circle(ctx, [x, y], color);
+          break;
+        }
+      }
+
+    });
+  }
 
   return {
+    init() {
+      paint();
+    },
     destroy() {
+      cancelAnimationFrame(raf);
       canvas.removeEventListener('mousemove', mousemove);
+      canvas.removeEventListener('mouseleave', mouseleave);
     }
   }
 }
@@ -57,6 +99,7 @@ function yAxis(ctx, yMin, yMax) {
   const stepText = (yMax - yMin)/ ROWS_COUNT;
 
   ctx.beginPath();
+  ctx.lineWidth = 1;
   ctx.strokeStyle = '#bbb';
   ctx.font = 'normal 20px Helvetica, sans-serif';
   ctx.fillStyle='#96a2aa';
@@ -73,15 +116,24 @@ function yAxis(ctx, yMin, yMax) {
   ctx.closePath();
 }
 
-function xAxis(ctx, data, xRatio) {
+function xAxis(ctx, data, xRatio, {mouse}) {
   const colsCount = 6;
   const step = Math.round(data.length / colsCount);
   ctx.beginPath();
-  for (let i = 1; i < data.length; i+= step) {
-    const text = toDate(data[i]);
+  for (let i = 1; i < data.length; i++) {
     const x = i * xRatio;
-    ctx.fillText(text.toString(), x, DPI_HEIGHT - 10);
+    if ((i - 1) % step === 0) {
+      const text = toDate(data[i]);
+      ctx.fillText(text.toString(), x, DPI_HEIGHT - 10);
+    }
+    if(isOver(mouse, x, data.length)) {
+     ctx.save();
+     ctx.moveTo(x, PADDING / 2);
+     ctx.lineTo(x, DPI_HEIGHT - PADDING);
+     ctx.restore();
+    } 
   }
+  ctx.stroke();
   ctx.closePath();
 }
 
@@ -97,8 +149,6 @@ function line(ctx, coords, {color}) {
   ctx.stroke();
   ctx.closePath();
 }
-
-chart(document.getElementById('chart'), getChartData());
 
 function computeBoundaries({columns, types}) {
     let min;
@@ -510,5 +560,23 @@ function toDate(timestamp) {
   const date = new Date(timestamp);
   
   return `${shortMonths[date.getMonth()]} ${date.getDate()}`;
+}
+
+function isOver(mouse, x, length) {
+  if(!mouse) {
+    return false
+  }
+  const width = DPI_WIDTH / length;
+  return Math.abs(x - mouse.x) < width / 2;
+}
+
+function circle(ctx, [x, y], color) {
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.fillStyle = '#fff';
+  ctx.arc(x, y, CIRCLE_RADIUS, 0, Math.PI *2);
+  ctx.fill();
+  ctx.stroke();
+  ctx.closePath();
 }
   
